2bccec3fa7f8a1a40e8ca80b56e52f2ebf88874a
rdeloffe <draphael59400@gmail.com>
2024-03-11 11:02:20 +0100

    page-inscription

--- script.js
+++ script.js
@@ -4,17 +4,20 @@ window.onload = function() {
         // Empêcher le comportement par défaut du formulaire qui est de soumettre les données
         event.preventDefault();
 
-        // Récupération des valeurs des mots de passe
-        var password = document.getElementById("password").value;
-        var confirmPassword = document.getElementById("confirmPassword").value;
-        
         // Vérification de la correspondance entre les mots de passe
-        if (password !== confirmPassword) {
-            // Afficher un message d'erreur si les mots de passe ne correspondent pas
-            alert("Les mots de passe ne correspondent pas !");
-        } else {
-            // Redirection vers la page acceuil.html après la soumission du formulaire
-            window.location.href = "acceuil.html";
-        }
+        checkPasswordMatch();
     });
+}
+
+function checkPasswordMatch() {
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
+    var passwordMatchError = document.getElementById("passwordMatchError");
+    
+    if (password !== confirmPassword) {
+        passwordMatchError.textContent = "Les mots de passe ne correspondent pas.";
+    } else {
+        // Si les mots de passe correspondent, soumettre le formulaire
+        document.getElementById("inscriptionForm").submit();
+    }
 }
